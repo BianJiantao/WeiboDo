05b586b9e84d38f295a3ba90b7856d40fbc181c0
Bjt <jiantaobian@qq.com>
2017-07-11 22:33:36 +0800

    使用一个函数封装AFN 的的GET /POST请求

--- JTWeibo/JTWeibo/Classes/Home(首页)/View(视图和控制器)/WBHomeViewController.swift
+++ JTWeibo/JTWeibo/Classes/Home(首页)/View(视图和控制器)/WBHomeViewController.swift
@@ -33,6 +33,24 @@ class WBHomeViewController: WBBaseViewController {
     // 加载数据
     override func loadData() {
         
+        // 用网络工具加载微博数据
+        let urlStr = "https://api.weibo.com/2/statuses/home_timeline.json"
+        let params = ["access_token":"2.00WKSdoG6a1eKB389a618b50dvgDPB"]
+        
+//        WBNetworkManager.shared.get(urlStr, parameters: params, progress: nil, success: { (_, json) in
+//            print(json)
+//            }) { (_, error) in
+//                print("网络请求失败\(error)")
+//        }
+        
+        WBNetworkManager.shared.request(URLString: urlStr, parameters: params) { (json, isSuccess) in
+            
+            print(json)
+            
+        }
+        
+        
+        
         /// 模拟'延时'加载数据
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
             
--- JTWeibo/JTWeibo/Classes/Main/Tools/Network/WBNetworkManager.swift
+++ JTWeibo/JTWeibo/Classes/Main/Tools/Network/WBNetworkManager.swift
@@ -9,10 +9,47 @@
 import UIKit
 import AFNetworking
 
+enum WBHTTPMethod {
+    case GET
+    case POST
+}
+
 class WBNetworkManager: AFHTTPSessionManager {
 
     // 单例  --- 静态区/常量 , 闭包
     // 第一次访问时执行闭包,并且将结果用 shared常量 保存
     static let shared = WBNetworkManager()
+    
+    /// 使用一个函数封装AFN 的的GET /POST请求
+    ///
+    /// - parameter method:     GET /POST
+    /// - parameter URLString:  URLString
+    /// - parameter parameters: 参数字典
+    /// - parameter completion: 请求的回调代码
+    func request(method:WBHTTPMethod = .GET,URLString: String, parameters:[String:Any],completion:@escaping (_ json:Any?,_ isSuccess:Bool)->()){
+        
+        // 成功的回调
+        let success = { (task:URLSessionTask , json:Any?)->() in
+            
+            completion(json, true)
+        }
+        // 失败的回调
+        let failure = {(task:URLSessionTask? , error:Error)->() in
+            print("网络请求错误\(error)")
+            completion(nil, false)
+        }
+        
+        if method == .GET {
+            
+            get(URLString, parameters: parameters, progress: nil, success: success, failure: failure)
+        }else{
+            
+            post(URLString, parameters: parameters, progress: nil, success: success, failure: failure)
+        }
+        
+    
+   
+    }
+    
     
 }
