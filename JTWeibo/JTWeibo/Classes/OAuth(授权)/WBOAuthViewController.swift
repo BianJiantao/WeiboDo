11ccf7673cf8363be91c91cd0ac2caa0c4525871
Bjt <jiantaobian@qq.com>
2017-07-13 15:36:55 +0800

    授权页面,自动填充用户名密码

--- JTWeibo/JTWeibo/Classes/OAuth(授权)/WBOAuthViewController.swift
+++ JTWeibo/JTWeibo/Classes/OAuth(授权)/WBOAuthViewController.swift
@@ -19,11 +19,12 @@ class WBOAuthViewController: UIViewController {
         
         navigationItem.title = "登录微博"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回", fontSize: 16, target: self, action: #selector(back), isBack: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "自动填充", target:  self, action: #selector(autoFill))
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // OAuth2 授权页面 url
         let urlStr = "https://api.weibo.com/oauth2/authorize?client_id=\(WBAppKey)&redirect_uri=\(WBAppRedirectUri)"
         guard let url = URL(string: urlStr) else{
@@ -35,15 +36,19 @@ class WBOAuthViewController: UIViewController {
         webView.loadRequest(request)
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
     
     // MARK: - 监听方法
+    // 自动填充用户名密码 -- webView 的注入 
+    @objc fileprivate func autoFill(){
+        
+        let js = "document.getElementById('userId').value = '2314373794@qq.com';" + "document.getElementById('passwd').value = 'test13579';"
+        //让webView 执行js
+        webView.stringByEvaluatingJavaScript(from: js)
+    }
+    
+    // 返回按钮点击
     @objc fileprivate func back(){
         dismiss(animated: true, completion: nil)
     }
