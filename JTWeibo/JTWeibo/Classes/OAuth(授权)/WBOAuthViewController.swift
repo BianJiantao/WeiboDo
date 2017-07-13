894a042d6f5ef0adfef11939fceeb9496d23ca4d
Bjt <jiantaobian@qq.com>
2017-07-13 16:15:27 +0800

    获取授权码

--- JTWeibo/JTWeibo/Classes/OAuth(授权)/WBOAuthViewController.swift
+++ JTWeibo/JTWeibo/Classes/OAuth(授权)/WBOAuthViewController.swift
@@ -14,7 +14,7 @@ class WBOAuthViewController: UIViewController {
     
     override func loadView() {
         view = webView
-        
+        webView.delegate = self
         view.backgroundColor = UIColor.white
         
         navigationItem.title = "登录微博"
@@ -40,7 +40,7 @@ class WBOAuthViewController: UIViewController {
 
     
     // MARK: - 监听方法
-    // 自动填充用户名密码 -- webView 的注入 
+    // 自动填充用户名密码 -- webView 的注入
     @objc fileprivate func autoFill(){
         
         let js = "document.getElementById('userId').value = '2314373794@qq.com';" + "document.getElementById('passwd').value = 'test13579';"
@@ -55,3 +55,33 @@ class WBOAuthViewController: UIViewController {
 
 
 }
+
+
+extension WBOAuthViewController:UIWebViewDelegate{
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        if request.url?.absoluteString.hasPrefix(WBAppRedirectUri) == false { // 请求的 url 不包含回调地址
+            
+            return true
+        }
+        
+        // query 时 url 中 ' ? ' 后面的内容   ( client_id=)
+        // https://api.weibo.com/oauth2/authorize?client_id=  (query :  client_id=)
+//        print(request.url?.query)
+        
+        if request.url?.query?.hasPrefix("code=") == false {
+            
+            print("取消授权")
+            back()
+            return false
+        }
+        
+        // 获取授权码
+        let code = request.url?.query?.substring(from: "code=".endIndex) ?? ""
+//        print("获取授权码--\(code)")
+        
+        return false
+    }
+}
+
